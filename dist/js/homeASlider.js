f3997b2d3634931752ecab57e82cf7843d821229
derek <derek4477@hotmail.com>
2020-04-03 19:14:28 -0700

    Feature and clock

--- dist/js/homeASlider.js
+++ dist/js/homeASlider.js
@@ -13,3 +13,28 @@ var swiper = new Swiper(".swiper-container", {
     prevEl: ".swiper-button-prev"
   }
 });
+const countDate = new Date("May 1, 2020 00:00:00").getTime();
+const newYear = () => {
+  let now = new Date().getTime();
+  let gap = countDate - now;
+  let secound = 1000;
+  let minute = secound * 60;
+  let hour = minute * 60;
+  let day = hour * 24;
+  let d = Math.floor(gap / day);
+  let h = Math.floor((gap % day) / hour);
+  let m = Math.floor((gap % hour) / minute);
+  let s = Math.floor((gap % minute) / secound);
+  console.log(d);
+  document.getElementById("day").innerText = d;
+  document.getElementById("hour").innerText = h;
+  document.getElementById("minute").innerText = m;
+  document.getElementById("secound").innerText = s;
+  document.querySelector(".day").innerHTML = `${d} Days`;
+  document.querySelector(".hour").innerHTML = `${h} Hours`;
+  document.querySelector(".minute").innerHTML = `${m} Mins`;
+  document.querySelector(".secound").innerHTML = `${s} Sec`;
+};
+setInterval(function() {
+  newYear();
+}, 1000);
